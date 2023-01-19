0fa78fe574b38e25b33df6604c64fcb87b7c31f4
eatmorevegetabl <moises@Moisess-MacBook-Pro.local>
2023-01-19 00:09:43 -0700

    another error

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const api = require('../Note-Taker/routes/index');
-const html = require('../Note-Taker/routes/index');
+const api = require('./routes/index');
+const html = require('./routes/index');
 
 const PORT = process.env.PORT || 3001;
 
