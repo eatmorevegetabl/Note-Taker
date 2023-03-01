48e5a9f584cf831bfcbf9582870194b4a8e07a1d
eatmorevegetabl <moises@Moisess-MacBook-Pro.local>
2023-03-01 01:19:48 -0700

    corrected line 8 to fs.readFile

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,10 +1,11 @@
 const router = require('express').Router();
 const composeNote = require('../helpers/fsPath');
+var fs = require('fs');
 const { notes } = require('../db/db.json');
 const { v4: uuidv4 } = require('uuid');
 
 router.get('/notes', (req, res) => {
-  readFromFile(notes).then((data) => res.json(JSON.parse(data)));
+  fs.readFile(notes).then((data) => res.json(JSON.parse(data)));
   // let results = notes;
   // res.json(results);
 });
